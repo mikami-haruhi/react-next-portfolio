d3d8944f470dea145757e767d4320d528dd3d1a6
三上春陽 <ktc24a31e0014@edu.kyoto-tech.ac.jp>
2025-01-25 16:14:00 +0900

    修正完了

--- app/blog/search/page.tsx
+++ app/blog/search/page.tsx
@@ -1,6 +1,6 @@
-import { getNewsList } from "@/app/_libs/microcms";
-import { NEWS_LIST_LIMIT } from "@/app/_constants";
-import NewsList from "@/app/_components/Bloglist";
+import { getBlogList } from "@/app/_libs/microcms";
+import { BLOG_LIST_LIMIT } from "@/app/_constants";
+import BlogList from "@/app/_components/Bloglist";
 import SearchField from "@/app/_components/SearchField";
 
 type Props = {
@@ -10,15 +10,15 @@ type Props = {
 };
 
 export default async function Page({ searchParams }: Props) {
-    const { contents:news} = await getNewsList({
-        limit: NEWS_LIST_LIMIT,
+    const { contents:blog} = await getBlogList({
+        limit: BLOG_LIST_LIMIT,
         q: searchParams.q,
     });
 
     return (
         <>
         <SearchField />
-        <NewsList news={news} />
+        <BlogList blog={blog} />
         </>
     );
 }
